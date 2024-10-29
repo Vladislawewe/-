6e7265c8c72d6cd5c46907311c5ffc41864f1718
Vladislawewe <145099909+Vladislawewe@users.noreply.github.com>
2024-10-30 01:14:25 +0500

    Update hello_world.asm

--- labs/laba4/task1/hello_world.asm
+++ labs/laba4/task1/hello_world.asm
@@ -1,8 +1,10 @@
-.ORIG x3000
+.ORIG x3000    
 
-LEA 	R0, HW
-PUTS
-HALT
+LEA  R0, HW    ; Загружаем адрес строки "Hello, World!" в регистр R0
+PUTS       ; Выводим строку на консоль
 
-HW	.STRINGZ "Hello, World!"
-	.END
+HALT       ; Завершаем выполнение программы
+
+HW .STRINGZ "Hello, World!"  ; Определение строки "Hello, World!"
+
+.END       ; Конец программы
