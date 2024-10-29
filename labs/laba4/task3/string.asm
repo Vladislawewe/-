240dfd296807c3bf9fd3035021769bb1969eb713
Vladislawewe <145099909+Vladislawewe@users.noreply.github.com>
2024-10-30 01:16:26 +0500

    Update string.asm

--- labs/laba4/task3/string.asm
+++ labs/laba4/task3/string.asm
@@ -1,28 +1,27 @@
-.ORIG x3000
+.ORIG x3000  
 
 LOOP
-	LEA	R0, INPUT
-	PUTS
-	LEA	R1, BUFF
-	ADD	R2, R1, #0
+ LEA R0, INPUT  ; Загружаем адрес строки "Your str: " в регистр R0
+ PUTS       ; Выводим строку на консоль
+ LEA R1, BUFF   ; Загружаем адрес буфера BUFF в регистр R1
+ ADD R2, R1, #0  ; Копируем адрес буфера в регистр R2 (R2 будет использоваться как индекс)
 
 LOOP_READ
-	GETC
-	OUT
-	ADD	R4, R0, #-10
-	BRz 	LOOP_PRINT
-	STR, 	R0, R2, #0
-	ADD	R2, R2, #1
-	BRnzp	LOOP_READ
+ GETC       ; Считываем символ с клавиатуры
+ OUT        ; Выводим считанный символ на консоль
+ ADD R4, R0, #-10 ; Проверяем, не был ли введен символ ENTER (ASCII код 10)
+ BRz  LOOP_PRINT  ; Если ENTER, переходим к выводу
+ STR,  R0, R2, #0 ; Сохраняем символ в буфер BUFF по адресу R2
+ ADD R2, R2, #1  ; Увеличиваем индекс R2 для следующего символа
+ BRnzp LOOP_READ ; Переходим к следующему символу
 
 LOOP_PRINT
-	LEA	R0, BUFF
-	PUTS
+ LEA R0, BUFF   ; Загружаем адрес буфера BUFF в регистр R0
+ PUTS       ; Выводим строку из буфера на консоль
 
-HALT
+HALT       ; Завершаем выполнение программы
 
-INPUT	.STRINGZ "Your str: "
-BUFF	.BLKW #80			
-	
-.END
+INPUT .STRINGZ "Your str: " ; Определение строки "Your str: "
+BUFF .BLKW #80   ; Определение буфера для хранения строки (80 байт)
 
+.END       ; Конец программы
